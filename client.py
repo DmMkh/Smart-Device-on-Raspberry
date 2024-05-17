6f6c81b4007af5333952caa8de07a0c18a689ffc
Ульяна Панкратова <gugush284@gmail.com>
2024-05-17 23:53:31 +0300

    http GET users and orders

---
+++ client.py
@@ -0,0 +1,30 @@
+import requests
+from const import addr
+
+def getUsers():
+    response = requests.get(addr + '/users')
+
+    return response.json()
+
+def getCart(user: int):
+    response = requests.get(addr + '/ready/' + str(user))
+
+    return response.json() 
+
+if __name__ == "__main__":
+    users = getUsers()
+
+    print(users)
+
+    if users != None:
+        for user in users:
+            order = getCart(user)
+
+            for elem in order:
+                print("Elem:")
+                print("It is package number: ", elem[0])
+                print("It is goods id: ", elem[1])
+                print("It is amount of good: ", elem[2])
+                print()
+
+    
