14fb829bc96b0ae1ac736bac1ab1c7c56c20a20b
Guguch <gugush284@gmail.com>
2024-06-08 14:42:22 +0300

    Return amount

--- main.py
+++ main.py
@@ -61,63 +61,6 @@ def main(args):
                 for line in order:
                     print(line)
 
-                
-                    if (line[1] == 1):
-                        image = Image.open('1.jpg')
-                    if (line[1] == 2):
-                        image = Image.open('2.jpg')
-                    if (line[1] == 3):
-                        image = Image.open('3.jpg')
-                    if (line[1] == 4):
-                        image = Image.open('4.jpg')
-                    if (line[1] == 5):
-                        image = Image.open('5.jpg')
-                    if (line[1] == 6):
-                        image = Image.open('6.jpg')
-                    image.show()
-                    while(j == 1):
-                        if (GPIO.input(27) == 0 and k  != 2):
-                            #print("hap")
-                            k = 2
-                            #GPIO.output(17, GPIO.HIGH)
-                            #time.sleep(0.5)
-                            Buzz(3000, 0.1)
-                            #GPIO.output(17, GPIO.LOW)
-                        if (k == 2 and GPIO.input(27) == 1):
-                            #print("hop")
-                            j = 2
-                    j = 1
-                    k = 1
-                    image.close()
-                    if (line[0] == '1'):
-                        GPIO.output(22, GPIO.LOW)
-                        GPIO.output(23, GPIO.HIGH)
-                        GPIO.output(24, GPIO.LOW)
-                        GPIO.output(25, GPIO.LOW)
-                    if (line[0] == '2'):
-                        GPIO.output(22, GPIO.LOW)
-                        GPIO.output(23, GPIO.LOW)
-                        GPIO.output(24, GPIO.HIGH)
-                        GPIO.output(25, GPIO.LOW)
-                    if (line[0] == '3'):
-                        GPIO.output(22, GPIO.HIGH)
-                        GPIO.output(23, GPIO.LOW)
-                        GPIO.output(24, GPIO.LOW)
-                        GPIO.output(25, GPIO.LOW)
-                    if (line[0] == '4'):
-                        GPIO.output(22, GPIO.LOW)
-                        GPIO.output(23, GPIO.LOW)
-                        GPIO.output(24, GPIO.LOW)
-                        GPIO.output(25, GPIO.HIGH)
-                    time.sleep(1)
-
-                delete(user)
-
-                time.sleep(3)
-
-                for line in order:
-                    print(line)
-
                     for i in range(1, line[2] + 1):
                         if (line[1] == 1):
                             image = Image.open('1.jpg')
@@ -166,8 +109,11 @@ def main(args):
                             GPIO.output(23, GPIO.LOW)
                             GPIO.output(24, GPIO.LOW)
                             GPIO.output(25, GPIO.HIGH)
-                        time.sleep(1)
-                    time.sleep(3)
+                    time.sleep(1)
+
+            delete(user)
+
+            time.sleep(3)
 
         GPIO.cleanup()
     
